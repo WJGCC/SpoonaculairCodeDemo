0847cc131a1ef3b2cda32fbf16ac157451888bff
WJGCC <wesley.jansen4@student.graafschapcollege.nl>
2024-09-13 11:32:58 +0200

    reformatting of the javascript files, api key changed, background color of body changed abd some needed format fixes, html left unchanged

--- ingredients.js
+++ ingredients.js
@@ -5,9 +5,19 @@ $(document).ready(function () {
     $.ajax({
         url: `https://api.spoonacular.com/recipes/${id}/information?apiKey=c25e2d349ba842ee8186ded1ff30b942`
     }).done(function (data) {
-        $('.recipe').append(`<h3 class="recipe-name">${data.title}</h3><img src="${data.image}"></img>`);
+        $('.recipe').append(`
+            <h3 class="recipe-name">${data.title}</h3>
+            <img src="${data.image}" />
+        `);
+
         data.extendedIngredients.forEach(ingredient => {
-            $('.ingredients').append(`<div class="ingredient"><h3>${ingredient.name}</h3><img src="https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}"></img><p>${ingredient.amount} ${ingredient.unit}</p></div>`);
+            $('.ingredients').append(`
+                <div class="ingredient">
+                    <h3>${ingredient.name}</h3>
+                    <img src="https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}" />
+                    <p>${ingredient.amount} ${ingredient.unit}</p>
+                </div>
+            `);
         });
     });
 });
